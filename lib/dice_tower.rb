6eaff05dc91a861afa49ab894b91f322a37967db
Miranda Wilson <mirandascwilson@gmail.com>
2021-01-07 21:05:02 +0000

    added a DiceTower class that rolls Dice

--- lib/dice_tower.rb
+++ lib/dice_tower.rb
@@ -1,6 +1,12 @@
+require "dice"
+
 class DiceTower
 
-  def add_dice
+  def roll_dice(roll_this_many_times)
+    result =[]
+    die = Dice.new
+    roll_this_many_times.times { result << die.roll }
+    result
   end
 
 end
--- spec/dice_tower_spec.rb
+++ spec/dice_tower_spec.rb
@@ -2,7 +2,12 @@ require "dice_tower"
 
 describe DiceTower do
 
-  it { is_expected.to respond_to :add_dice }
+  it { is_expected.to respond_to(:roll_dice).with(1).argument }
   
+  it "rolls dice" do
+    die = Dice.new
+    result = subject.roll_dice(3)
+    expect(result.length).to eq 3
+  end
   
 end
